445fb51c7e89f407db412d743afcb36f19ae50b6
Renato Bueno <37316420+Renatobuen0@users.noreply.github.com>
2020-09-20 23:29:20 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Example from './components/Example';
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<Example />
+			</header>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/Example.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Example.css';
+const Example = () => {
+	return (
+		<div className="container">
+			<img alt="demo-monday-icon" src="./icons/android-icon-192x192.png" />
+			<span>this is the first part of monday demo app as progressive web app example</span>
+		</div>
+	);
+};
+
+export default Example;
