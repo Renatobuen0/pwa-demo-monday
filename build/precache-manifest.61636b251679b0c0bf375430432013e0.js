aaa84ad657415c6c4a119c7ba0ea013f9bafa98a
Renato Bueno <37316420+Renatobuen0@users.noreply.github.com>
2020-09-20 23:38:13 -0300

    added build files

---
+++ build/precache-manifest.61636b251679b0c0bf375430432013e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65f3087c028aa55715e47840ca391119",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee334acb3b9837928648",
+    "url": "/static/css/main.89dd1a66.chunk.css"
+  },
+  {
+    "revision": "7efa7f41e772c9936f1e",
+    "url": "/static/js/2.e1eb555d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e1eb555d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee334acb3b9837928648",
+    "url": "/static/js/main.3661906e.chunk.js"
+  },
+  {
+    "revision": "75be02485cca283ed4fb",
+    "url": "/static/js/runtime-main.3be6a693.js"
+  }
+]);
